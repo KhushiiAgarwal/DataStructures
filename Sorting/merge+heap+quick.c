1c0c3313ff06b23db6479e710224505b038910a7
UdayJaju24 <112578423+UdayJaju24@users.noreply.github.com>
2024-01-09 13:00:37 +0530

    Update merge+heap+quick.c

--- Sorting/merge+heap+quick.c
+++ Sorting/merge+heap+quick.c
@@ -1,5 +1,6 @@
-#include &lt;stdio.h&gt;
-#include &lt;stdlib.h&gt;
+#include <stdio.h>
+#include <stdlib.h>
+
 struct Employee {
     char employee_name[20];
     int emp_no;
@@ -13,39 +14,39 @@ void swap(struct Employee *a, struct Employee *b){
 int partition(struct Employee arr[] , int s , int e , int *swap_count){
     int pivot = arr[s].emp_no;
     int count = 0;
-    for (int i = s+1; i &lt;= e; i++)
+    for (int i = s+1; i <= e; i++)
     {
-        if (arr[i].emp_no &lt;= pivot)
+        if (arr[i].emp_no <= pivot)
         {
             count++;
         }      
 
     }
     int pivot_index = s + count;
-    swap(&amp;arr[pivot_index] , &amp;arr[s]);
+    swap(&arr[pivot_index] , &arr[s]);
     (*swap_count)++;
     int k = s;
     int l = e;
-    while (k &lt; pivot_index &amp;&amp; l &gt; pivot_index)
+    while (k < pivot_index && l > pivot_index)
     {
-        if (arr[k].emp_no &lt;= pivot)
+        if (arr[k].emp_no <= pivot)
         {
             k++;
         }
-        if (arr[l].emp_no &gt; pivot)
+        if (arr[l].emp_no >pivot)
         {
             l--;
         }
-        if (k &lt; pivot_index &amp;&amp; l &gt; pivot_index)
+        if (k < pivot_index && l>pivot_index)
         {
-            swap(&amp;arr[k++] , &amp;arr[l--]);
+            swap(&arr[k++] , &arr[l--]);
             (*swap_count)++;
         }      
     }  
     return pivot_index;  
 }
 void quick_sort(struct Employee arr[] , int s , int e , int*swap_count){
-    if (s &gt;= e)
+    if (s >= e)
     {
         return;
     }
@@ -63,21 +64,21 @@ void merge(struct Employee arr[] , int s , int e ){
     struct Employee second[length2];
    
     int mai = s;
-    for (int i = 0; i &lt; length1; i++)
+    for (int i = 0; i < length1; i++)
     {
         first[i] = arr[mai++];
     }
     mai = m+1;
-    for (int i = 0; i &lt; length2; i++)
+    for (int i = 0; i < length2; i++)
     {
         second[i] = arr[mai++];
     }
     int index1 = 0;
     int index2 = 0;
     mai = s;
-    while (index1 &lt; length1 &amp;&amp; index2 &lt; length2)
+    while (index1 < length1 && index2 < length2)
     {
-        if (first[index1].emp_no &lt; second[index2].emp_no)
+        if (first[index1].emp_no < second[index2].emp_no)
         {
             arr[mai++] = first[index1++];
             count++;
@@ -87,20 +88,19 @@ void merge(struct Employee arr[] , int s , int e ){
             arr[mai++] = second[index2++];
         }        
     }
-    while (index1 &lt; length1)
+    while (index1 <length1)
     {
         arr[mai++] = first[index1++];
     }
-    while (index2 &lt; length2)
+    while (index2 < length2)
     {
         arr[mai++] = second[index2++];
     }
-    printf(&quot;\nAnd number of swap performed using Merge Sort: %d\n&quot; ,
-count);
+    printf("\nAnd number of swap performed using Merge Sort: %d\n" ,count);
 }
 void merge_sort(struct Employee arr[] , int s , int e){
 
-    if (s&gt;= e)
+    if (s>= e)
     {
         return;
     }
@@ -113,62 +113,57 @@ void heapify(struct Employee arr[], int n, int i) {
     int largest = i;
     int l = 2*i + 1;
     int r = 2*i + 2;
-    if (l &lt; n &amp;&amp; arr[l].emp_no &gt; arr[largest].emp_no)
+    if (l < n && arr[l].emp_no > arr[largest].emp_no)
         largest = l;
-    if (r &lt; n &amp;&amp; arr[r].emp_no &gt; arr[largest].emp_no)
+    if (r < n && arr[r].emp_no > arr[largest].emp_no)
         largest = r;
     if (largest != i) {
-        swap(&amp;arr[i], &amp;arr[largest]);
+        swap(&arr[i], &arr[largest]);
         heapify(arr, n, largest);
     }
 }
 void heapSort(struct Employee arr[], int n) {
     int count = 0;
-    for (int i = n / 2 - 1; i &gt;= 0; i--)
+    for (int i = n / 2 - 1; i >= 0; i--)
         heapify(arr, n, i);
-    for (int i=n-1; i&gt;=0; i--) {
-        swap(&amp;arr[0], &amp;arr[i]);
+    for (int i=n-1; i>=0; i--) {
+        swap(&arr[0], &arr[i]);
         count++;
         heapify(arr, i, 0);
     }
-    printf(&quot;And number of swap performed using Heap Sort: %d\n&quot;, count);
+    printf("And number of swap performed using Heap Sort: %d\n", count);
 }
 void main(){
 
    
     struct Employee arr[3];
-        for (int i =0; i&lt;3 ; i++){
-        printf(&quot;Enter employee name , employee number and employee salary
-%d\n&quot;,(i+1));
-        scanf(&quot;%s %d %d&quot;,&amp;arr[i].employee_name,&amp;arr[i].emp_no,
-&amp;arr[i].emp_salary);
+        for (int i =0; i<3 ; i++){
+        printf("Enter employee name , employee number and employee salary%d\n",(i+1));
+        scanf("%s %d %d",&arr[i].employee_name,&arr[i].emp_no,arr[i].emp_salary);
         }
     int n = sizeof(arr)/sizeof(arr[0]);
     int swap_count = 0;
     int s = 0;
     int e = n-1;
-    quick_sort(arr , s , e , &amp;swap_count);
-    printf(&quot;Sorted Array using Quick Sort: \n&quot;);
-    for (int i = 0; i &lt; n; i++)
+    quick_sort(arr , s , e , &swap_count);
+    printf("Sorted Array using Quick Sort: \n");
+    for (int i = 0; i < n; i++)
     {
-        printf(&quot;%s %d %d \n&quot; , arr[i].employee_name , arr[i].emp_no ,
+        printf("%s %d %d \n", arr[i].employee_name , arr[i].emp_no ,
 arr[i].emp_salary);
     }
-    printf(&quot;And number of swap performed using Quick Sort: %d\n&quot; ,
-swap_count);
+    printf("And number of swap performed using Quick Sort: %d\n" ,swap_count);
     merge_sort(arr , s , e);
-    printf(&quot;\nSorted Array using Merge Sort: \n&quot;);
-    for (int i = 0; i &lt; n; i++)
+    printf("\nSorted Array using Merge Sort: \n");
+    for (int i = 0; i< n; i++)
     {
-        printf(&quot; %s %d %d \n&quot; , arr[i].employee_name , arr[i].emp_no ,
-arr[i].emp_salary);
+        printf(" %s %d %d \n", arr[i].employee_name , arr[i].emp_no ",arr[i].emp_salary);
     }
    
     heapSort(arr, n);
-    printf(&quot;Sorted Array using Heap Sort:\n&quot;);
-    for (int i=0; i&lt;n; i++) {
-        printf(&quot; %s %d %d \n&quot; , arr[i].employee_name , arr[i].emp_no ,
-arr[i].emp_salary);    
+    printf("Sorted Array using Heap Sort:\n");
+    for (int i=0; i<n; i++) {
+        printf(" %s %d %d \n" , arr[i].employee_name , arr[i].emp_no ,arr[i].emp_salary);    
         }
     return 0;
 }
